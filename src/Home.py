127cdc83a61924c48fcf9d916795ae5f49e9b9db
Vaibhav Chopda <vaibhavchopda04gmail.com>
2023-09-21 18:54:43 +0100

    Added AI conversation page

--- src/Home.py
+++ src/Home.py
@@ -1,31 +1,31 @@
 import streamlit as st
 from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import FAISS
 import os
 from dotenv import load_dotenv
 
 
-#Config
+# Config
 st.set_page_config(layout="wide", page_icon="🤖", page_title="AI | Chat-Bot")
 
-#Title
+# Title
 st.markdown(
     """
     <h2 style='text-align: center;'> Your AI Conversation assistant 🤖</h1>
     """,
-    unsafe_allow_html=True,)
+    unsafe_allow_html=True,
+)
 
 st.markdown("---")
 
-#Robby's Pages
+# Robby's Pages
 st.subheader("🚀 AI Conversation KIT's Pages")
-st.write("""
+st.write(
+    """
 - **Chat**: General Chat on data (CSV) with a [vectorstore](https://github.com/facebookresearch/faiss) (index useful parts(max 4) for respond to the user) | works with [ConversationalRetrievalChain](https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html)
-""")
+"""
+)
 st.markdown("---")
 
 
@@ -34,15 +34,18 @@ if os.path.exists(".env"):
     user_api_key = os.getenv("OPENAI_API_KEY")
     st.sidebar.success("API key loaded from .env", icon="🚀")
     st.session_state.api_key = user_api_key
-    
+
 else:
     user_api_key = st.sidebar.text_input(
-    label="#### Your OpenAI API key 👇",
-    placeholder="Paste your openAI API key, sk-",
-    type="password")
+        label="#### Your OpenAI API key 👇",
+        placeholder="Paste your openAI API key, sk-",
+        type="password",
+    )
 
 with st.sidebar.expander("📬 Contact"):
-    st.write("**GitHub:**",
-"[vaibhav-chopda/AIConversationKit](https://github.com/vaibhav-chopda/AIConversationKit.git)")
+    st.write(
+        "**GitHub:**",
+        "[vaibhav-chopda/AIConversationKit](https://github.com/vaibhav-chopda/AIConversationKit.git)",
+    )
     st.write("**Mail** : vaibhavchopda04@gmail.com")
     st.write("**Created by Vaibhav Chopda**")
--- src/embedding/embedder.py
+++ src/embedding/embedder.py
@@ -1,7 +1,8 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
+
 def generate_embedding(data):
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(data, embeddings)
-    return vectorstore
+    return embeddings, vectorstore
